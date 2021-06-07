c25e2481cc2a776dc1345b4b4dfc59401d6a2868
folathecoder <officialakinloyefolarin@gmail.com>
2021-06-07 09:28:10 +0100

    Refactored JavaScript Code

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,7 +1,6 @@
 'use strict';
 
 //TODO: Declare all required variables
-
 const hamburgerOpen = document.querySelector('.hamburger--open');
 const hamburgerClose = document.querySelector('.hamburger--close');
 const hamburger = document.querySelector('.hamburger');
@@ -9,32 +8,36 @@ const overlay = document.querySelector('.overlay');
 const menu = document.querySelector('.menu');
 const body = document.body;
 
-
+//* Add class to hide gamburger close icon and overlay
 hamburgerClose.classList.add('hidden');
 overlay.classList.add('hidden');
-// menu.classList.add('menu--active');
 
-hamburgerOpen.addEventListener('click', e => {
+//* Functions that opens and closes the dropdown menu on mobile
+const openDropdown = function() {
     hamburgerOpen.classList.add('hidden');
     hamburgerClose.classList.remove('hidden');
     overlay.classList.remove('hidden');
     menu.classList.remove('menu--active');
     body.style.overflow = 'hidden';
-    
-})
+} 
 
+const closeDropdown = function() {
+    hamburgerOpen.classList.remove('hidden');
+    hamburgerClose.classList.add('hidden');
+    overlay.classList.add('hidden');
+    menu.classList.add('menu--active');
+    body.style.overflow = 'auto';
+} 
+
+//* Dropdown Open
+hamburgerOpen.addEventListener('click', e => {
+    openDropdown();   
+});
+//* Dropdown Close
 hamburgerClose.addEventListener('click', e => {
-    hamburgerOpen.classList.remove('hidden');
-    hamburgerClose.classList.add('hidden');
-    overlay.classList.add('hidden');
-    menu.classList.add('menu--active');
-    body.style.overflow = 'auto';
+    closeDropdown();
 })
-
+//* Dropdown Close when the overlay is clicked
 overlay.addEventListener('click', e => {
-    hamburgerOpen.classList.remove('hidden');
-    hamburgerClose.classList.add('hidden');
-    overlay.classList.add('hidden');
-    menu.classList.add('menu--active');
-    body.style.overflow = 'auto';
+    closeDropdown();
 })
